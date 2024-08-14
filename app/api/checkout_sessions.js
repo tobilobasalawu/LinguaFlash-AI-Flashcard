15557eb0dcb4210de44ec41de9d074c2051fe436
tobilobasalawu <oluwatobilobasalawu1234@gmail.com>
2024-08-14 17:31:49 +0100

    Implementing the GET route

--- app/api/checkout_sessions.js
+++ app/api/checkout_sessions.js
@@ -51,3 +51,21 @@ export async function POST(req) {
     })
   }
 }
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try {
+    if (!session_id) {
+      throw new Error('Session ID is required')
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+
+    return NextResponse.json(checkoutSession)
+  } catch (error) {
+    console.error('Error retrieving checkout session:', error)
+    return NextResponse.json({ error: { message: error.message } }, { status: 500 })
+  }
+}
