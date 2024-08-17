6bd72d2054c481631d13878b340f7b7415f87f95
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2024-08-18 00:45:56 +0100

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        "tiro-devanagari-hindi": "Tiro Devanagari Hindi",
+        "dela-gothic-one": "Dela Gothic One",
+        manrope: "Manrope",
+        caramel: "Caramel",
       },
     },
   },
   plugins: [],
-};
+}
