89c0ab3a3df5ae7d649e986e9098d4dade8e21eb
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2024-08-21 12:40:03 +0100

    add SEO meta tags

--- app/layout.js
+++ app/layout.js
@@ -40,8 +40,57 @@ export default function RootLayout({ children }) {
           <head>
             <meta
               name="description"
-              content="Master languages faster with AI-generated flashcards tailored for
-            your learning needs for free!"
+              content="Master languages faster with AI-generated flashcards tailored for your learning needs for free!"
+            />
+            <meta
+              name="robots"
+              content="index, follow"
+            />
+            <meta
+              name="author"
+              content="Oyelola Victoria, Anushka Narssima, Oluwatobi Salawu"
+            />
+            <meta
+              name="keywords"
+              content="LinguaFlash AI, Flashcards, AI generated flashcards, language learning, learn a language, learn Spanish, learn French, learn German, language study, AI flashcard app, custom flashcards"
+            />
+
+            <meta
+              property="og:title"
+              content="LinguaFlash AI"
+            />
+            <meta
+              property="og:description"
+              content="Master languages faster with AI-generated flashcards tailored for your learning needs for free!"
+            />
+            <meta
+              property="og:image"
+              content="https://linguaflashai.vercel.app/seo.png"
+            />
+            <meta
+              property="og:url"
+              content="https://linguaflashai.vercel.app"
+            />
+            <meta
+              property="og:type"
+              content="website"
+            />
+
+            <meta
+              name="twitter:card"
+              content="summary_large_image"
+            />
+            <meta
+              name="twitter:title"
+              content="LinguaFlash AI"
+            />
+            <meta
+              name="twitter:description"
+              content="Master languages faster with AI-generated flashcards tailored for your learning needs for free!"
+            />
+            <meta
+              name="twitter:image"
+              content="https://linguaflashai.vercel.app/seo.png"
             />
             <title>LinguaFlash AI</title>
             <link
